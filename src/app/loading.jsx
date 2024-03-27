0a60d78862a85dd1705eb2543adfd05699f638cc
SukhpreetO1 <ssingh77022@gmail.com>
2024-03-27 11:38:23 +0530

    adding loader page

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  console.log("loading page");
+  return (
+    <>
+      <div className="loader-container">
+        <div className="loader"></div>
+      </div>
+    </>
+  );
+}
