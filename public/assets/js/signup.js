2cedf6befc95128d1910bcb0dc1733b387bf65be
SukhpreetO1 <ssingh77022@gmail.com>
2024-03-27 22:06:53 +0530

    change in the validation messages

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -2,55 +2,64 @@ export const validate_signup_submit_form = (data) => {
     const errors = {};
 
     if (!data.first_name.trim()) {
-        errors.first_name = 'First name is required';
+        errors.first_name = 'First name is required.';
     } else if (!/^[a-zA-Z]+$/.test(data.first_name)){
-        errors.first_name = 'First name should contain only letters';
+        errors.first_name = 'First name should contain only letters.';
     }
 
     if (!data.last_name.trim()) {
-        errors.last_name = 'Last name is required';
+        errors.last_name = 'Last name is required.';
     } else if (!/^[a-zA-Z]+$/.test(data.last_name)){
-        errors.last_name = 'Last name should contain only letters';
+        errors.last_name = 'Last name should contain only letters.';
     }
     
     if (!data.email.trim()) {
-        errors.email = 'Email is required';
+        errors.email = 'Email is required.';
     } else if (!/\S+@\S+\.\S+/.test(data.email)) {
-        errors.email = 'Invalid email format. Format must be like abc@gmail.com';
+        errors.email = 'Invalid email format.';
     }
 
     if (!data.username.trim()) {
-        errors.username = 'Username is required';
+        errors.username = 'Username is required.';
     } else if (!/^(?=.*[a-zA-Z])[a-zA-Z0-9]+$/.test(data.username)){
-        errors.username = 'Username must contain letters and numbers only';
+        errors.username = 'Username must contain letters and numbers only.';
     }
 
+    const currentDate = new Date();
+    const inputDate = new Date(data.date_of_birth);
+
     if (!data.date_of_birth.trim()) {
-        errors.date_of_birth = 'Date of birth is required';
+        errors.date_of_birth = 'Date of birth is required.';
+    } else if (!/^\d{4}-\d{2}-\d{2}$/.test(data.date_of_birth)) {
+        errors.date_of_birth = 'Invalid date format.';
+    } else if (inputDate > currentDate) {
+        errors.date_of_birth = 'Date of birth cannot be greater than today.';
     }
 
     if (!data.mobile_number.trim()) {
-        errors.mobile_number = 'Mobile number is required';
+        errors.mobile_number = 'Mobile number is required.';
     } else if (!/^\d{10,12}$/.test(data.mobile_number)){
         errors.mobile_number = 'Mobile number must contain 10 to 12 numbers only.';
     }
 
     if (!data.gender.trim()) {
-        errors.gender = 'Gender is required';        
+        errors.gender = 'Gender is required.';  
     }
 
     if (!data.hobbies) {
-        errors.hobbies = 'Hobby field is required';        
+        errors.hobbies = 'Hobby field is required.';  
+    } else if (data.hobbies == '') {
+        errors.hobbies = 'Please select atleast 1 hobby.';
     }
 
     if (!data.password.trim()) {
-        errors.password = 'Password is required';
+        errors.password = 'Password is required.';
     } else if(!/^(?=.*\d)(?=.*[a-z]|[A-Z]).{6,20}$/.test(data.password)) {
         errors.password = 'Invalid password format. Must contain at least 6 characters, 1 capital letter and 1 number.';
     }
 
     if (!data.confirm_password.trim()) {
-        errors.confirm_password = 'Confirm password is required';
+        errors.confirm_password = 'Confirm password is required.';
     } else if (data.confirm_password != data.password) {
         errors.confirm_password = 'Confirm password does not match.';
     }
