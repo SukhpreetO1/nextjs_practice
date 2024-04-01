4538e2fe6e92271a1493e3010708de1a73967455
SukhpreetO1 <ssingh77022@gmail.com>
2024-04-01 13:58:21 +0530

    adding the loader in blog, dashboard, contact form pages

--- src/app/admin/blogs/page.jsx
+++ src/app/admin/blogs/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon, faPenToSquare, faTrashCan, faInfo, faPlus, Link, ADMIN_ADD_BLOGS, toast, ADMIN_DASHBOARD, ADMIN_EDIT_BLOGS, doc, db, deleteDoc, ADMIN_BLOG_MODAL, ADMIN_BLOGS, Image, getFirestore, getDocs, collection } from '@/app/api/routes/page';
+import { FontAwesomeIcon, faPenToSquare, faTrashCan, faInfo, faPlus, Link, ADMIN_ADD_BLOGS, toast, ADMIN_DASHBOARD, ADMIN_EDIT_BLOGS, doc, db, deleteDoc, ADMIN_BLOG_MODAL, ADMIN_BLOGS, Image, getFirestore, getDocs, collection, Loader } from '@/app/api/routes/page';
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
@@ -8,6 +8,7 @@ const Blogs = () => {
     const [showModal, setShowModal] = useState(false);
     const [selectedBlogId, setSelectedBlogId] = useState(null);
     const [isChecked, setIsChecked] = useState();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         if (localStorage.getItem("hasShownBlogAddedToast") === "false") {
@@ -23,9 +24,11 @@ const Blogs = () => {
         }
 
         async function fetchData() {
+            setLoading(true);
             const response = await fetch("/api/blogs");
             const data = await response.json();
             setBlogs(data.data);
+            setLoading(false);
         }
 
         fetchData();
@@ -46,6 +49,11 @@ const Blogs = () => {
         const initialCheckedState = blogs.map((blog) => blog.dashboard_visible === 1 ? false : true);
         setIsChecked(initialCheckedState);
     }, [blogs]);
+
+    const modalHandler = () => {
+        setShowModal(false);
+        setBlogModalDetail("")
+    };
 
     const handleCheckboxChange = async (e, blogId) => {
         const dashboard_visible_value = e.target.checked ? 1 : 2;
@@ -128,95 +136,100 @@ const Blogs = () => {
     return (
         <>
             <section>
-                <div className="blogs_page ml-60">
-                    <div className="heading text-center text-5xl font-bold mt-8 mb-12">
-                        Blogs
-                    </div>
+                {loading ? (
+                    <Loader />
+                ) : (
+                    <div className="blogs_page ml-60">
+                        <div className="heading text-center text-5xl font-bold mt-8 mb-12">
+                            Blogs
+                        </div>
 
-                    <div className="add_blogs_page absolute right-24 top-20">
-                        <Link href={ADMIN_ADD_BLOGS} >
-                            <FontAwesomeIcon icon={faPlus} className='w-8 h-8' />
-                        </Link>
-                    </div>
+                        <div className="add_blogs_page absolute right-24 top-20">
+                            <Link href={ADMIN_ADD_BLOGS} >
+                                <FontAwesomeIcon icon={faPlus} className='w-8 h-8' />
+                            </Link>
+                        </div>
 
-                    <div className="relative overflow-x-auto">
-                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                <tr>
-                                    <th scope="col" className="px-6 py-3 w-1/6">
-                                        Blog name
-                                    </th>
-                                    <th scope="col" className="px-6 py-3 w-2/3">
-                                        Description
-                                    </th>
-                                    <th scope="col" className="px-6 py-3">
-                                        Action
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {blogs.map((blog, index) => (
-                                    <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                        <td className="px-6 py-4">{blog.title}</td>
-                                        <td className="px-6 py-4">{truncateDescription(blog.description)}</td>
-                                        <td className="px-6 py-4 flex">
-                                            <Link href="#" onClick={() => { setShowModal(true); setSelectedBlogId(blog.id); }}>
-                                                <FontAwesomeIcon icon={faInfo} className="w-4 h-4 mr-2 mt-1" />
-                                            </Link>
-                                            <Link href={`${ADMIN_EDIT_BLOGS}/${blog.id}`}>
-                                                <FontAwesomeIcon icon={faPenToSquare} className="w-4 h-4 mr-2 mt-1" />
-                                            </Link>
-                                            <Link href="#" onClick={() => deleteBlog(blog.id)}>
-                                                <FontAwesomeIcon icon={faTrashCan} className="w-4 h-4 mr-2 mt-1" />
-                                            </Link>
-                                            <div className="toggle_button">
-                                                <label className="inline-flex items-center cursor-pointer">
-                                                    <input type="checkbox" value={blog.dashboard_visible} className="sr-only peer" checked={isChecked[index]} onChange={(e) => handleCheckboxChange(e, blog.id)} />
-                                                    <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                                </label>
-                                            </div>
-                                        </td>
+
+                        <div className="relative overflow-x-auto">
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3 w-1/6">
+                                            Blog name
+                                        </th>
+                                        <th scope="col" className="px-6 py-3 w-2/3">
+                                            Description
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Action
+                                        </th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                        {blogs?.length < 1 && <div className="py-2 text-center text-xl">No data found</div>}
-                        {showModal && (
-                            <div id="default-modal" tabIndex="-1" aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-80">
-                                <div className="relative p-4 w-4/6 max-h-full blog_modal">
-                                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                                            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                                                Blog Details
-                                            </h3>
-                                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={() => setShowModal(false)}>
-                                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                </svg>
-                                            </button>
-                                        </div>
-                                        <div className="blog_details py-8 px-8">
-                                            <div className='flex mb-4'>
-                                                <div className="uploaded_image mr-4">
-                                                    <Image src={blogModalDetail.image} alt="Uploaded Image" width={400} height={100} />
+                                </thead>
+                                <tbody>
+                                    {blogs.map((blog, index) => (
+                                        <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <td className="px-6 py-4">{blog.title}</td>
+                                            <td className="px-6 py-4">{truncateDescription(blog.description)}</td>
+                                            <td className="px-6 py-4 flex">
+                                                <Link href="#" onClick={() => { setShowModal(true); setSelectedBlogId(blog.id); }}>
+                                                    <FontAwesomeIcon icon={faInfo} className="w-4 h-4 mr-2 mt-1" />
+                                                </Link>
+                                                <Link href={`${ADMIN_EDIT_BLOGS}/${blog.id}`}>
+                                                    <FontAwesomeIcon icon={faPenToSquare} className="w-4 h-4 mr-2 mt-1" />
+                                                </Link>
+                                                <Link href="#" onClick={() => deleteBlog(blog.id)}>
+                                                    <FontAwesomeIcon icon={faTrashCan} className="w-4 h-4 mr-2 mt-1" />
+                                                </Link>
+                                                <div className="toggle_button">
+                                                    <label className="inline-flex items-center cursor-pointer">
+                                                        <input type="checkbox" value={blog.dashboard_visible} className="sr-only peer" checked={isChecked[index]} onChange={(e) => handleCheckboxChange(e, blog.id)} />
+                                                        <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                                    </label>
                                                 </div>
-
-                                                <div className="title">
-                                                    <p className='break-words text-justify italic text-2xl leading-loose mb-4'>{blogModalDetail.title}</p>
-                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                            {blogs?.length < 1 && <div className="py-2 text-center text-xl">No data found</div>}
+                            {showModal && (
+                                <div id="default-modal" tabIndex="-1" aria-hidden="true" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-80">
+                                    <div className="relative p-4 w-4/6 max-h-full blog_modal">
+                                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                                    Blog Details
+                                                </h3>
+                                                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={modalHandler}>
+                                                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                    </svg>
+                                                </button>
                                             </div>
-                                            <div className="description">
-                                                <div className="description-content break-words">
-                                                    <p className='break-words text-justify font-light leading-loose text-base'>{blogModalDetail.description}</p>
+                                            <div className="blog_details py-8 px-8">
+                                                <div className='flex mb-4'>
+                                                    <div className="uploaded_image mr-4">
+                                                        <Image src={blogModalDetail.image} alt="Uploaded Image" width={400} height={100} />
+                                                    </div>
+
+                                                    <div className="title">
+                                                        <p className='break-words text-justify italic text-2xl leading-loose mb-4'>{blogModalDetail.title}</p>
+                                                    </div>
+                                                </div>
+                                                <div className="description">
+                                                    <div className="description-content break-words">
+                                                        <p className='break-words text-justify font-light leading-loose text-base'>{blogModalDetail.description}</p>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        )}
+                            )}
+                        </div>
                     </div>
-                </div>
+                )}
             </section>
         </>
     )
--- src/app/admin/contact_form_messages/page.jsx
+++ src/app/admin/contact_form_messages/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { ADMIN_DASHBOARD, FontAwesomeIcon, Image, Link, faInfo, faPenToSquare } from "@/app/api/routes/page";
+import { ADMIN_DASHBOARD, FontAwesomeIcon, Image, Link, Loader, faInfo, faPenToSquare } from "@/app/api/routes/page";
 import { useEffect, useState } from "react";
 
 const ContactFormMessages = () => {
@@ -7,12 +7,15 @@ const ContactFormMessages = () => {
     const [contactMessagesDetail, setContactMessagesDetail] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [selectedContactMessagesId, setSelectedContactMessagesId] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         async function fetchData() {
+            setIsLoading(true);
             const response = await fetch("/api/contact_messages");
             const data = await response.json();
             setContactMessages(data.data);
+            setIsLoading(false);
         }
         fetchData();
     }, [])
@@ -22,12 +25,16 @@ const ContactFormMessages = () => {
             async function fetchData(modal_id) {
                 const response = await fetch("/api/contact_messages/contact_messages_modal/" + modal_id);
                 const data = await response.json();
-                console.log(data);
                 setContactMessagesDetail(data.data);
             }
             fetchData(selectedContactMessagesId);
         }
     }, [selectedContactMessagesId]);
+
+    const modalHandler = () => {
+        setShowModal(false);
+        setContactMessagesDetail('');
+    }
 
     const truncateContactMessage = (message, limit) => {
         if (message.length > limit) {
@@ -40,6 +47,7 @@ const ContactFormMessages = () => {
     return (
         <>
             <section>
+                {isLoading ? ( <Loader /> ) : (
                 <div className="contact_form_messages_page ml-60">
                     <div className="heading text-center text-5xl font-bold mt-8 mb-12">
                         Contact Form Messages
@@ -104,7 +112,7 @@ const ContactFormMessages = () => {
                                             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                                                 Contact Form Message Details
                                             </h3>
-                                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={() => setShowModal(false)}>
+                                            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal" onClick={modalHandler}>
                                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                                 </svg>
@@ -133,6 +141,7 @@ const ContactFormMessages = () => {
                         )}
                     </div>
                 </div>
+                )}
             </section>
         </>
     )
